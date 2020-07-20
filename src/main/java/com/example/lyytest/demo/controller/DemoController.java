d2aa050b6286d7f68fe04dd6b53e8964e8085482
liuyangyang <821533794@qq.com>
2020-07-20 16:31:06 +0800

    1231312

--- src/main/java/com/example/lyytest/demo/controller/DemoController.java
+++ src/main/java/com/example/lyytest/demo/controller/DemoController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @GetMapping
     public String test(){
-        return "说点什么吧";
+        return "说点什么吧123";
     }
 }
